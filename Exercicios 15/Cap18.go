9945bc7e1e8850942c2126f4320734972cd720cc
MaiconGavino <maiconserrao57@gmail.com>
2021-02-05 09:55:21 -0400

    commit Cap 18

---
+++ Exercicios 15/Cap18.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	fmt.Println(runtime.NumCPU())
+	fmt.Println(runtime.NumGoroutine())
+
+	wg.Add(2)
+	go loop1()
+	go loop2()
+
+	fmt.Println(runtime.NumGoroutine())
+	wg.Wait()
+}
+func loop1() {
+	for i := 0; i <= 100; i++ {
+		fmt.Println("loop1", i)
+		time.Sleep(20)
+	}
+	wg.Done()
+}
+func loop2() {
+	for i := 0; i <= 100; i++ {
+		fmt.Println("loop2", i)
+		time.Sleep(20)
+	}
+	wg.Done()
+}
