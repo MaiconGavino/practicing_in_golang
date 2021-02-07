10f6d71b8b42cd38cd3bddee3aff8139395ac042
MaiconGavino <maiconserrao57@gmail.com>
2021-02-07 17:16:51 -0400

    actualization

--- Exercicios 16/Pratica3.go
+++ Exercicios 16/Pratica3.go
@@ -1,7 +1,7 @@
 //Chans par, ímpar, quit
 // Func send manda números pares pra um canal, ímpares pra outro, e fecha/quit
 // Func receive é um select entre os três canais, encerra no quit
-package main
+/*package main
 
 import (
 	"fmt"
@@ -32,7 +32,53 @@ func apresentar(par chan int, impar chan int, quit chan int) {
 			fmt.Println("o valor", v, "é par")
 		case v := <-impar:
 			fmt.Println("o valor", v, "é impar")
+			//quando fechei os canais preciso usar o comma ok
 		case <-quit:
+			return
+		}
+	}
+}
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	par := make(chan int)
+	impar := make(chan int)
+	quit := make(chan int)
+	go num(par, impar, quit)
+	apresentar(par, impar, quit)
+
+}
+func num(par chan int, impar chan int, quit chan int) {
+	for i := 1; i <= 50; i++ {
+		if i%2 == 0 {
+			par <- i
+		} else {
+			impar <- i
+		}
+	}
+	close(par)
+	close(impar)
+	quit <- 0
+}
+func apresentar(par chan int, impar chan int, quit chan int) {
+	for {
+		select {
+		case v := <-par:
+			fmt.Println("o valor", v, "é par")
+		case v := <-impar:
+			fmt.Println("o valor", v, "é impar")
+			//quando fechei os canais preciso usar o comma ok
+		case v, ok := <-quit:
+			if !ok {
+				fmt.Println("Deu erro", v)
+			} else {
+				fmt.Println("Encerando... Recebemos:", v)
+			}
 			return
 		}
 	}
