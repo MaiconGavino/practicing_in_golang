258ddf8938646f5b867234dcc6c602d126e05203
MaiconGavino <maiconserrao57@gmail.com>
2021-02-12 11:08:50 -0400

    add exerc3

---
+++ Exercicios 16/exerc3.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	q := make(chan int)
+	c := gen(q)
+
+	receive(c, q)
+
+	fmt.Println("about to exit")
+}
+
+func gen(q chan<- int) <-chan int {
+	c := make(chan int)
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		close(c)
+		q <- 0
+	}()
+
+	return c
+}
+func receive(canal <-chan int, quit chan int) {
+	for {
+		select {
+		case v := <-canal:
+			fmt.Println(v)
+		case <-quit:
+			return
+		}
+
+	}
+}
