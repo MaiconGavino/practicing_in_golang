34d1ac4ebf3064d2084cfc26e3cb2becba832731
maicongavino <maiconserrao57@gmail.com>
2022-02-16 11:26:57 -0300

    Atualizações

--- Exercicios 19/calc/calc.go
+++ Exercicios 19/calc/calc.go
@@ -2,9 +2,13 @@ package calc
 
 import "errors"
 
-func Div(value1, value2 float64) (float64, error) {
+func Divisao(value1, value2 float64) (float64, error) {
 	if value2 == 0 {
 		return 0, errors.New("Não existe divisão por zero")
 	}
 	return value1 / value2, nil
 }
+
+func Multiplicacao(value1, value2 float64) (float64){return value1 * value2}
+func Soma(value1, value2 float64) (float64){return value1 + value2}
+func Subtracao(value1, value2 float64) (float64){return value1 - value2}
--- Exercicios 19/main.go
+++ Exercicios 19/main.go
@@ -1,27 +1,59 @@
 package main
 
-import (ca
+import (
+	"calc"
 	"fmt"
 )
 
 func main() {
+	valor1, valor2 := imput()
+	esc := escolhas()
+	operacao(valor1, valor2, esc)
+}
+func imput() (float64, float64) {
 	var value1, value2 float64
 	fmt.Println("======== Vamos Calcular ===========")
 	fmt.Println("Informe o Primeiro Valor: ")
 	fmt.Scan(&value1)
 	fmt.Println("Informe o Segundo Valor: ")
 	fmt.Scan(&value2)
+	return value1, value2
+}
+func escolhas() int {
 	fmt.Println("======== Escolha a Operação ===========")
 	fmt.Println("[0] - Soma")
-	fmt.Println("[1] - Substração")
+	fmt.Println("[1] - Subtração")
 	fmt.Println("[2] - Multiplicação")
 	fmt.Println("[3] - Divisão")
 	var escolha int
 	fmt.Scan(escolha)
-	result, err := calc.Div(value1, value2)
-	fmt.Println(result)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+	return escolha
+}
+
+func operacao(valor1, valor2 float64, escolha int) {
+
+	switch {
+	case escolha == '0':
+		result, _ := calc.Soma(value1, value2)
+		fmt.Println("A soma de %f.2 e %f.2 é: ", valor1, valor2, result)
+
+	case escolha == '1':
+		result, _ := calc.Subtracao(value1, value2)
+		fmt.Println("A subtração de %f.2 e %f.2 é: ", valor1, valor2, result)
+
+	case escolha == '2':
+		result, _ := calc.Multiplicacao(value1, value2)
+		fmt.Println("A multiplicação de %f.2 e %f.2 é: ", valor1, valor2, result)
+
+	case escolha == '3':
+		result, err := calc.Divisao(value1, value2)
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Println("A divisão de %f.2 e %f.2 é: ", valor1, valor2, result)
+		}
+
+	default:
+		fmt.Println("Escolha Invalida")
 	}
 }
